058d0e67b0933273e8242096f115fd55dfe19eca
Galang Aji Susanto <galang.aji@mekari.com>
2024-08-25 20:51:19 +0700

    Add string catalog for localizations and regions

--- LocalizationApp/ContentView.swift
+++ LocalizationApp/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let name = "Galang"
+    let applicationCount = 6
+    let yearExperience: Double = 3.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading, spacing: 20) {
+            Text("Hello, world! My name is \(name)")
+            
+            Text("I love coding")
+        
+            Text("After \(yearExperience, specifier: "%.1f") year(s) experience, i have released \(applicationCount) application(s)")
+
         }
         .padding()
     }

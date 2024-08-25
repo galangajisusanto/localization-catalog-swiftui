5e0c7bac9a558e7fb8b5ed9c74574fc348dd5798
Galang Aji Susanto <galang.aji@mekari.com>
2024-08-25 11:44:49 +0700

    Initial Commit

---
+++ LocalizationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalizationApp
+//
+//  Created by Mekari on 25/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalizationApp/LocalizationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationAppApp.swift
+//  LocalizationApp
+//
+//  Created by Mekari on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
